57a1315b8b8dfb04b4f97c6f8642541feffd506b
Salvo 'LtWorf' Tomaselli <tiposchi@tiscali.it>
2016-01-31 00:30:19 +0100

    Return m as documented, not Atm
    The function was documented to return in meters but was
    actually calculating in Atm.

--- calc.cpp
+++ calc.cpp
@@ -4,11 +4,13 @@
 
 double maximum_operative_depth(double ean) {
     double o2 = ean / 100;
-    return MAX_PO2 / o2;
+    double pressure = MAX_PO2 / o2;
+    return (pressure - 1) * 10;
 }
 
 double best_operative_depth(double ean, unsigned int factors) {
     double o2 = ean/100;
     double margin = (factors+1) / 10;
-    return (MAX_PO2 - margin) / o2;
+    double pressure = (MAX_PO2 - margin) / o2;
+    return (pressure - 1) * 10;
 }
